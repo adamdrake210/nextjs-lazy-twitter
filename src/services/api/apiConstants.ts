2b6e61255f3820a8bd69f25a94d35d2458be4f7b
Adam Drake <adamgedrake@gmail.com>
2022-03-18 11:34:35 +0100

    installing some packages (#1)
    * installing some packages
    
    * cleanup
    
    Co-authored-by: Adam Drake <addrake@ebay.com>

---
+++ src/services/api/apiConstants.ts
@@ -0,0 +1,21 @@
+export const API_ENDPOINTS = {
+  // USER API ENDPOINTS
+
+  API_USER_LOGIN: {
+    method: 'POST',
+    url: 'auth/login'
+  },
+  API_USER_REGISTER: {
+    method: 'POST',
+    url: 'register'
+  },
+  API_USER_PROFILE: {
+    method: 'GET',
+    url: 'profile'
+  },
+  API_GET_ONE_USER: {
+    method: 'GET',
+    url: 'user'
+  }
+
+}
